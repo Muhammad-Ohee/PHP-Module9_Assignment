3ed3f7606b050e254be1bb8fb847d9c10113138a
Muhammad Ohee <md.ohee786@gmail.com>
2023-11-26 14:52:12 +0600

    version 1

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <ul>
+        <li><a href="#githublink#">GitHub</a></li>
+        <li><a href="#Youtubelink">Youtube</a></li>
+        <!-- Add more social media links as needed -->
+    </ul>
+</footer>
